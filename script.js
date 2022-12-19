28c3ae96a288fcb09da19814dcb1329aebc11372
lakshmi <lakshmi.raavi2016@gmail.com>
2022-12-19 16:40:20 +0530

    carousel changes done

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ $(document).ready(function(){
   $('#featureCarousel').carousel({interval: false});
   $('#featureCarousel').carousel('pause');
 });
+
+let items2 = document.querySelectorAll('#featureContainer .carousel .carousel-item');
+items2.forEach((el) => {
+  const minPerSlide2 = 6
+  let next2 = el.nextElementSibling
+  for (var i=1; i<minPerSlide2; i++) {
+    if (!next2) {
+      // wrap carousel by using first child
+      next2 = items2[0]
+    }
+    let cloneChild2 = next2.cloneNode(true)
+    el.appendChild(cloneChild2.children[0])
+    next2 = next2.nextElementSibling
+  }
+})
+$(document).ready(function(){
+  $('#featureCarousel').carousel({interval: false});
+  $('#featureCarousel').carousel('pause');
+});
